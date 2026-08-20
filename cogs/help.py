08a066ed4eedb08dde862178fa8c2237bd0e32c7
nayrux <nayrux@atomicmail.io>
2026-08-19 20:55:42 -0600

    Add Help cog with help command and embed

--- cogs/help.py
+++ cogs/help.py
@@ -1,1 +1,23 @@
+import discord
+from discord.ext import commands
+from config import Config
+from views.help_select import CompleteHelpView
+from utils.embeds import create_help_embed
 
+class Help(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.hybrid_command(name="help", description="Show help menu")
+    async def help(self, ctx, category: str = None):
+        if category:
+            await ctx.send(f"Help for {category}")
+            return
+        
+        embed = create_help_embed()
+        view = CompleteHelpView()
+        
+        await ctx.send(embed=embed, view=view)
+
+async def setup(bot):
+    await bot.add_cog(Help(bot))
